379ec2809d672e77e546292a6692ee456906e104
xinwendewen <zwt0909@outlook.com>
2021-04-21 00:29:32 +0800

    support repo sync projects

--- repo-functions.sh
+++ repo-functions.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 #
 #
-
 set -e
 set -u
 
 repo_sync() {
     local repo_dir=$1
     cd ${repo_dir}
-    repo sync -j 32 -c
+    shift
+    if [ $# -eq 0 ]; then
+        echo "sync all projects in dir ${repo_dir}"
+        repo sync -j 32 -c
+    else
+        echo "sync projects [$@] in dir ${repo_dir}"
+        repo sync -j 32 -c $@
+    fi
 }
+
