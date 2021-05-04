47e3cd25c99bd7ddaf48ce4c57b6e1b18b476bb9
xinwendewen <zwt0909@outlook.com>
2021-05-04 22:10:58 +0800

    remotable adb

--- adb-functions.sh
+++ adb-functions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 #
+
 current_package() {
     adb shell dumpsys activity recents | grep -w "Recent #0" | cut -d '=' -f 2 | cut -d ' ' -f 1
 }
@@ -12,3 +13,22 @@ pull() {
 clear_dir() {
     adb shell rm -rf "$1/*"
 }
+
+remotable_adb() {
+    set -u
+    local cmd=$1
+    set +u
+    if [[ -n ${HOST} && -n ${PORT} ]];
+    then
+        #echo "adb -H ${HOST} -P ${PORT} ${cmd}"
+        adb -H ${HOST} -P ${PORT} ${cmd}
+    else
+        #echo "adb ${cmd}"
+        adb ${cmd}
+    fi
+}
+
+main() {
+}
+
+main $@
