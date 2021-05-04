ecb0d388f9f6c2355e3a37a29d3ca4f6d4f16caa
xinwendewen <zwt0909@outlook.com>
2021-05-04 22:00:48 +0800

    modify repo sync

--- repo-functions.sh
+++ repo-functions.sh
@@ -4,25 +4,25 @@
 set -e
 set -u
 
-FAILED_REPOS_FILE=failed-repos
-
 repo_sync() {
-    local repo_dir=$1
-    cd ${repo_dir}
-    shift
-    if [ $# -eq 0 ]; then
-        echo "sync all projects in dir ${repo_dir}"
-        repo sync -j 32 -c 2>&1 >/dev/null | grep "Failing repos" > $FAILED_REPOS_FILE
-    else
-        echo "sync projects [$@] in dir ${repo_dir}"
-        repo sync -j 32 -c $@ 2>&1 >/dev/null | grep "Failing repos" > $FAILED_REPOS_FILE
+    local root_dir=$1
+    if [[ ! -d "$root_dir" ]];
+    then
+        echo "$root_dir is not a directory"
+        return 1
     fi
-    sed -i '1d' $FAILED_REPOS_FILE
-    echo "Total $(wc -l $FAILED_REPOS_FILE | awk '{print $1;}') failing repos:"
-    while read project
-    do
-        show_project ${repo_dir} ${project}
-    done < $FAILED_REPOS_FILE
+    cd ${root_dir}
+    shift
+    if [[ -z "$*" ]];
+    then
+        echo "sync all projects in $root_dir"
+        #echo "repo sync"
+        repo sync
+    else
+        echo "sync projects:[$*] in $root_dir"
+        #echo "repo sync $*"
+        repo sync "$*"
+    fi
 }
 
 show_project() {
@@ -32,3 +32,8 @@ show_project() {
     repo list $@
 }
 
+main() {
+    #repo_sync $@
+}
+
+main $@
