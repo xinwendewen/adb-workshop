6f275ec0ce537b7f64aacafa2201a3bf1612a6e6
xinwendewen <zwt0909@outlook.com>
2021-05-24 22:10:07 +0800

    add alias

--- adb-alias.sh
+++ adb-alias.sh
@@ -4,3 +4,5 @@ alias adb-ds='adb shell dumpsys'
 alias adb-ds-gfx='adb shell dumpsys gfxinfo'
 
 alias adb-ds-top-activity='adb shell dumpsys activity | grep top-activity'
+
+alias grep-system-service='adb shell dumpsys -l | grep'
